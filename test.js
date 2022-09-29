9f864d342e3f7b24b07f8314ff7203adac065619
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-09-29 15:39:22 -0400

    it works and random account works

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 const playwright = require('playwright');
+require('dotenv').config()
+
 async function main() {
     const browser = await playwright.firefox.launch({
         headless: false // setting this to true will not run the UI
@@ -7,7 +9,12 @@ async function main() {
     const page = await browser.newPage();
     await page.goto('https://www.kijiji.ca/t-login.html');
     await page.waitForTimeout(rn(3,5)); // wait for 5 seconds
-    await page.type("input#emailOrNickname", )
+    const i = Math.floor(Math.random()*process.env.USERNAMES.split(" ").length)
+    await page.type('input#emailOrNickname', process.env.USERNAMES.split(" ")[i]);
+    await page.waitForTimeout(rn(3,5));
+    await page.type('input#password', process.env.PASSWORDS.split(" ")[i]);
+    await page.waitForTimeout(rn(3,5));
+    await page.click('form#LoginForm >> button[type="submit"]');
     // await browser.close();
 }
 
