3f4b2fa40dbc04c17241c2b7338e37ea975182fa
Ch3ru09 <xliang0905@gmail.com>
2022-09-29 00:18:53 -0400

    can enter and log in

--- test.js
+++ test.js
@@ -8,6 +8,7 @@ async function main() {
 
     const page = await browser.newPage();
     await page.goto('https://www.kijiji.ca/t-login.html');
+<<<<<<< HEAD
     await page.waitForTimeout(rn(3,5)); // wait for 5 seconds
     const i = Math.floor(Math.random()*process.env.USERNAMES.split(" ").length)
     await page.type('input#emailOrNickname', process.env.USERNAMES.split(" ")[i]);
@@ -16,6 +17,13 @@ async function main() {
     await page.waitForTimeout(rn(3,5));
     await page.click('form#LoginForm >> button[type="submit"]');
     // await browser.close();
+=======
+    await page.type('input[id="emailOrNickname"]', "runcongliang@gmail.com", {delay: 100});
+    await page.waitForTimeout(1000)
+    await page.type('input[id="password"]', "terry78925", {delay: 100});
+    await page.waitForTimeout(1000)
+    await page.click('form#LoginForm >> button[type="submit"]');
+>>>>>>> 78f66ac (can enter and log in)
 }
 
 function rn(min, max) {
